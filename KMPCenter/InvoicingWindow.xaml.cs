09e4be39529e7ffd1b919681472b6c4406de1213
Lincoln Yu <quanben@msn.com>
2019-06-04 09:35:12 +1000

    changes to commit to further work.

--- KMPCenter/InvoicingWindow.xaml.cs
+++ KMPCenter/InvoicingWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Windows;
+using Window = System.Windows.Window;
 
 namespace KMPCenter
 {
@@ -10,6 +13,36 @@ namespace KMPCenter
         public InvoicingWindow()
         {
             InitializeComponent();
+        }
+
+        private void GenerateClick(object sender, RoutedEventArgs e)
+        {
+            Generate();
+        }
+
+        //https://www.c-sharpcorner.com/UploadFile/muralidharan.d/how-to-create-word-document-using-C-Sharp/
+        void Generate()
+        {
+            try
+            {
+                //Create an instance for word app  
+                var winword = new Microsoft.Office.Interop.Word.Application();
+
+                //Set animation status for word application  
+                winword.ShowAnimation = false;
+
+                //Set status for word application is to be visible or not.  
+                winword.Visible = false;
+
+                //Create a missing variable for missing value  
+                object missing = System.Reflection.Missing.Value;
+
+                //TODO ...
+            }
+            catch (Exception ex)
+            {
+                App.ShowMessage(ex.Message);
+            }
         }
     }
 }
