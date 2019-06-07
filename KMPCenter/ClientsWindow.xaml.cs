887f2ff4c4bba3cf75b3e4549aab2dacba3ad7a1
Lincoln Yu <quanben@msn.com>
2019-06-08 00:28:37 +1000

    invoice population wip.

---
+++ KMPCenter/ClientsWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace KMPCenter
+{
+    /// <summary>
+    /// Interaction logic for ClientsWindow.xaml
+    /// </summary>
+    public partial class ClientsWindow : Window
+    {
+        private MainWindow _mainwindow;
+
+        public ClientsWindow(MainWindow mw)
+        {
+            _mainwindow = mw;
+            InitializeComponent();
+        }
+    }
+}
