8039c9313145855ac60a58f34294e8d1a442fc17
Lincoln Yu <quanben@msn.com>
2019-06-08 18:16:12 +1000

    invoice gen mostly done.

--- KMPCenter/InvoicingWindow.xaml.cs
+++ KMPCenter/InvoicingWindow.xaml.cs
@@ -3,6 +3,7 @@ using Microsoft.Office.Interop.Word;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Windows;
 using Window = System.Windows.Window;
 
@@ -13,6 +14,8 @@ namespace KMPCenter
     /// </summary>
     public partial class InvoicingWindow : Window
     {
+        static object missing = System.Reflection.Missing.Value;
+
         private MainWindow _mainwindow;
 
         public InvoicingWindow(MainWindow mw)
@@ -25,9 +28,37 @@ namespace KMPCenter
 
         private void GenerateClick(object sender, RoutedEventArgs e)
         {
+            var svcs = GenerateRandomServices(24);
             Generate(@"c:\temp\output.docx", "Test client", "Test client No.", "Test claim no.", "test diagnosis detail",
-                "test receipt no.", DateTime.Today, "test healthfund", "test membership no.", new Service[]{ }, 
+                "test receipt no.", DateTime.Today, "test healthfund", "test membership no.", svcs.ToList(), 
                 30, 10, 20);
+        }
+        
+        private IEnumerable<Service> GenerateRandomServices(int count)
+        {
+            var rand = new Random();
+            for (var i = 0; i < count; i++)
+            {
+                var date = DateTime.Today;
+                var svcdetail = new StringBuilder("Sample Service");
+                var stringlen = rand.Next(3, 50);
+                for (var j = 0; j < stringlen; j++)
+                {
+                    var c = rand.Next(0, 26);
+                    svcdetail.Append('A' + c);
+                }
+                decimal total = (decimal)(rand.NextDouble() * 100);
+                yield return new Service {
+                    Date = date,
+                    Detail = svcdetail.ToString(),
+                    Total = total,
+                    Owing = total,
+                    Benefit = 0,
+                    Discount = 0,
+                    Gap = 0,
+                    Balance = total
+                };
+            }
         }
 
         //https://www.c-sharpcorner.com/UploadFile/muralidharan.d/how-to-create-word-document-using-C-Sharp/
@@ -51,7 +82,6 @@ namespace KMPCenter
                 object isVisible = false;
 
                 //Create a missing variable for missing value  
-                object missing = System.Reflection.Missing.Value;
                 object infilename = InvoiceTemplatePath;
                 var document = winword.Documents.Open(infilename, missing, readOnly, 
                     missing, missing, missing, missing, missing, missing, missing, 
@@ -73,11 +103,34 @@ namespace KMPCenter
                 t.Cell(14, 3).Range.Text = balance.ToString();
 
                 object rref = t.Cell(9, 1);
-                t.Rows.Add(ref rref);
-                t.Rows.Add(ref rref);
-                t.Rows.Add(ref rref);
-                t.Rows.Add(ref rref);
-                t.Rows.Add(ref rref);
+
+                var rn = 8;
+                foreach (var svc in services)
+                {
+                    t.Cell(rn, 1).Range.Text = svc.Date.ToShortDateString();
+                    t.Cell(rn, 2).Range.Text = svc.Detail;
+                    t.Cell(rn, 3).Range.Text = svc.Total.ToString();
+                    t.Cell(rn, 4).Range.Text = svc.Owing.ToString();
+                    t.Cell(rn, 5).Range.Text = svc.Benefit.ToString();
+                    t.Cell(rn, 6).Range.Text = svc.Gap.ToString();
+                    t.Cell(rn, 7).Range.Text = $"{svc.Discount}%";
+                    t.Cell(rn, 8).Range.Text = svc.Balance.ToString();
+
+                    t.Rows.Add(ref rref);
+                    rn++;
+                }
+
+                var origPagesCount = GetPagesCount(document);
+                while (true)
+                {
+                    t.Rows.Add(ref rref);
+                    var currPageCount = GetPagesCount(document);
+                    if (currPageCount > origPagesCount)
+                    {
+                        document.Undo();
+                        break;
+                    }
+                }
 
                 object filename = outputfilename;
                 document.SaveAs2(ref filename);
@@ -91,6 +144,13 @@ namespace KMPCenter
             {
                 App.ShowMessage(ex.Message);
             }
+        }
+
+        private static int GetPagesCount(Document document)
+        {
+            var PagesCountStat = WdStatistic.wdStatisticPages;
+            int pagesCount = document.ComputeStatistics(PagesCountStat, ref missing);
+            return pagesCount;
         }
     }
 }
